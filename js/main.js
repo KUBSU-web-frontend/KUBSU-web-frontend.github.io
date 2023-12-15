dfab3929691c8d1c9912cebf4573a4d96e452653
DariaDyak <144004029+DariaDyak@users.noreply.github.com>
2023-12-15 20:35:55 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,4 +1,25 @@
 $(document).ready(function () {
+
+    $('.reviews__slider').on(`init reInit`, function (event, slick) {
+        $('.slick-slide-num-current').text(1 + ' ');
+    })
+    $('.reviews__slider').on(`afterChange`, function (event, slick, currentSlide, nextSlide) {
+        $('.slick-slide-num-current').text((currentSlide + 1) + ' ');
+    })
+    $('.reviews__slider').slick({
+        arrows: true,
+        dots: false,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        adaptiveHeight: true,
+        easing: 'ease',
+        appendArrows: '.slick__arrows',
+        prevArrow: '<span class="slick-prev"><</span>',
+        nextArrow: '<span class="slick-next">></span>'
+
+    });
+    
     $('.partners__slider-1').slick({
         arrows: false,
         dots: false,
@@ -13,6 +34,15 @@ $(document).ready(function () {
             breakpoint: 480,
             settings: {
                 slidesToShow: 2,
+                slidesToScroll: 1,
+                centerMode: false,
+                variableWidth: true,
+            }
+        }],
+        responsive: [{
+            breakpoint: 1300,
+            settings: {
+                slidesToShow: 5,
                 slidesToScroll: 1,
                 centerMode: false,
                 variableWidth: true,
@@ -38,6 +68,15 @@ $(document).ready(function () {
                 centerMode: true,
                 focusOnSelect: true,
                 variableWidth: true
+            }
+        }],
+        responsive: [{
+            breakpoint: 1300,
+            settings: {
+                slidesToShow: 5,
+                slidesToScroll: 1,
+                centerMode: false,
+                variableWidth: true,
             }
         }],
     });
